cc8b198892e1ed9ab2eb6bfe92f5d9591dab3f3f
blowa <light06120602@gmail.com>
2025-04-15 17:26:07 +0900

    Add files via upload

---
+++ GitHubjavaScript.js
@@ -0,0 +1,17 @@
+document.getElementById('surveyForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const game = document.getElementById('game').value;
+
+  const status = document.getElementById('status');
+  status.textContent = "送信中…";
+  status.style.color = "gray";
+
+  setTimeout(() => {
+    console.log("送信内容:", { name, game });
+    status.textContent = "送信2成功しました！ご協力ありがとうございました。";
+    status.style.color = "green";
+    document.getElementById('surveyForm').reset();
+  }, 1200); // 送信ディレイ
+});
