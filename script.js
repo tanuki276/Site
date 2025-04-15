64206bcfd3909bf3ea3a4ba50c1aaabad7e16862
blowa <light06120602@gmail.com>
2025-04-15 17:59:30 +0900

    script.js の更新

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.getElementById('surveyForm').addEventListener('submit', function (e) {
 
   setTimeout(() => {
     console.log("送信内容:", { name, game });
-    status.textContent = "送信2成功しました！ご協力ありがとうございました。";
+    status.textContent = "送信に成功しました！ご協力ありがとうございました。";
     status.style.color = "green";
     document.getElementById('surveyForm').reset();
   }, 1200); // 送信ディレイ
